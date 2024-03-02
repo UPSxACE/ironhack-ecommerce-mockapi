ac772327e170395a0a5ef1958894b40be97e19cf
Eduardo Botelho <eduardo11224b@gmail.com>
2024-03-02 10:06:45 +0000

    cors

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 
 const rules = auth.rewriter({
   // Permission rules
