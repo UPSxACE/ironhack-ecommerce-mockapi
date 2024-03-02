a0228738e242eafdf9d44e87c66bfd74cc4fd65a
Eduardo Botelho <eduardo11224b@gmail.com>
2024-03-02 09:52:59 +0000

    server file

---
+++ server.js
@@ -0,0 +1,24 @@
+const jsonServer = require("json-server");
+const auth = require("json-server-auth");
+
+const rules = auth.rewriter({
+  // Permission rules
+  users: 600,
+  categories: 644,
+  products: 644,
+  rates: 644,
+  // Other rules
+  "/posts/:category": "/posts?category=:category",
+});
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+// /!\ Bind the router db to the app
+app.db = router.db;
+
+// You must apply the auth middleware before the router
+app.use(rules);
+app.use(auth);
+app.use(router);
+app.listen(process.env.PORT);
